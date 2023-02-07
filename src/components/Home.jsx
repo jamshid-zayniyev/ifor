162d00060e989be7c8d14f6aef6a3fe9b597ca50
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2023-02-07 20:05:43 +0500

    img
    img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="hero">
         <div className="card bg-dark text-white border-0">
-            <img src={backgroundimage} className="card-img" alt="Backgroundimage" height="550px"/>
+            <img src={backgroundimage} className="card-img" alt="Backgroundimage" height="700px"/>
             <div className="card-img-overlay d-flex flex-column justify-content-center">
                 <div className="container">
             <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
